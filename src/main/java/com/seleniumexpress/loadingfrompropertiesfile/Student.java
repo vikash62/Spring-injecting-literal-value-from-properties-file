b7182c9eb390ff7ea6765dbad901c2e9f7123715
Vikash <vikashpathak041@gmail.com>
2022-08-14 15:29:16 +0530

    first commit

--- src/main/java/com/seleniumexpress/loadingfrompropertiesfile/Student.java
+++ src/main/java/com/seleniumexpress/loadingfrompropertiesfile/Student.java
@@ -4,25 +4,13 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Student {
+	  @Value("${student.name}")
   private String name;
+	  @Value("${student.intrestedCourse}")
   private String intrestedCourse;
+	  @Value("football")
   private String hobby;
 
-  @Value("${student.name}")
-public void setName(String name) {   
-	this.name = name;
-}
- 
-  @Required
-  @Value("${student.intrestedCourse}")
-
-public void setIntrestedCourse(String intrestedCourse) {
-	this.intrestedCourse = intrestedCourse;
-}
-  @Value("football")
-public void setHobby(String hobby) { 
-	this.hobby = hobby;
-}
    public void displayStudentInfo() {
 	   System.out.println("student name "+ name);
 	   System.out.println("student intrestedCourse "+ intrestedCourse);
